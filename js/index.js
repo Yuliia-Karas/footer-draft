030be07507c6036262dd0e4f4ba5221ea8748ffa
Yuliia Karas <iuliiakaras@gmail.com>
2023-05-03 13:35:26 +0200

    arrow

--- js/index.js
+++ js/index.js
@@ -1,18 +1,52 @@
 // import "./io";
 
-const callback = entries => {
-  entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-      console.log("Hello, world!");
-      console.log(entry);
-    };
+// import './io';
+// const callback = (entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       console.log("Hello, world!");
+//       console.log(entry);
+//     }
+//   });
+// };
+
+// const options = {
+//   rootMargin: "200px",
+// };
+// const observer = new IntersectionObserver(callback, options);
+
+// const sentinel = document.querySelector("#sentinel");
+// observer.observe(sentinel);
+
+// /Стрілка/ 
+// const scrollToTop = document.querySelector(".scroll-to-top");
+
+// window.addEventListener("scroll", () => {
+//   if (window.pageYOffset > 100) {
+//     scrollToTop.style.display = "block";
+//   } else {
+//     scrollToTop.style.display = "none";
+//   }
+// });
+
+// або
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
-};
-
-const options = {
-  rootMargin: '200px',
-};
-const observer = new IntersectionObserver(callback, options);
-
-const sentinel = document.querySelector("#sentinel");
-observer.observe(sentinel);
--- js/io.js
+++ js/io.js
@@ -1,9 +1,10 @@
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//       console.log('Hello, world!');
-//     console.log(entry);
-//   });
-// });
 
-// const sentinel = document.querySelector("#sentinel");
-// observer.observe(sentinel);
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log('Hello, world!');
+    console.log(entry);
+  });
+});
+
+const sentinel = document.querySelector("#sentinel");
+observer.observe(sentinel);
